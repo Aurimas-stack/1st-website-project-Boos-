eba0229d0d391d2e1878e38f689de123399797d9
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 10:38:10 +0300

    Add files via upload

---
+++ views/pages/content.php
@@ -0,0 +1,227 @@
+<section class="recent">
+    <div class="container flex-container">
+        <div class="features flex-container">
+            <div class="project-window flex-container">
+                <img src="images/BusinessGrowth.jpg" alt="BusinessGrowth">
+                <p>Business Growth</p>
+                <p>Coaching</p>
+            </div>
+            <div class="project-window flex-container">
+                <img src="images/data_analytics.jpg" alt="Data_Analytics">
+                <p>Data Analytics</p>
+                <p>Duis aute irure dolo.</p>
+            </div>
+            <div class="project-window flex-container">
+                <img src="images/Revenue_Growth.jpg" alt="Revenue_Growth">
+                <p>Revenue Growth</p>
+                <p>Stackholder Relations</p>
+            </div>
+        </div>
+        <div class="features flex-container ">
+                <div class="project-window flex-container">
+                    <img src="images/BusinessGrowth.jpg" alt="BusinessGrowth">
+                    <p>Business Growth</p>
+                    <p>Coaching</p>
+                </div>
+                <div class="project-window flex-container">
+                    <img src="images/data_analytics.jpg" alt="Data_Analytics">
+                    <p>Data Analytics</p>
+                    <p>Duis aute irure dolo.</p>
+                </div>
+                <div class="project-window flex-container">
+                    <img src="images/Revenue_Growth.jpg" alt="Revenue_Growth">
+                    <p>Revenue Growth</p>
+                    <p>Stackholder Relations</p>
+                </div>
+        </div>
+    </div>
+    </section>
+    <section class="trust-success">
+        <div class="container flex-container">
+            <h2 class="section-titles">All of our customers trust their success to us</h2>
+            <p class="section-paragraph">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis.</p>
+        </div>
+    </section>
+    <section class="payment-plans">
+        <div class="container flex-container">
+            <div class="payment-plan-box flex-container">
+                <p>Starter</p>
+                <p>Free</p>
+                <p>for up to 2 editors and 3 projects</p>
+                <div class="paymen-plan-benefits">
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>3 projects</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>30-day version history</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Up to 2 editors</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Unlimited cloud storage</p>
+                    </div>
+                </div>
+                <button class="yellow-btn" type="button">Get Now</button>
+            </div>
+            <div class="payment-plan-box flex-container">
+                <p>Professional</p>
+                <p>$9.99/mo</p>
+                <p>for up to 2 editors and 3 projects</p>
+                <div class="paymen-plan-benefits">
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>3 projects</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>30-day version history</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Up to 2 editors</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Unlimited cloud storage</p>
+                    </div>
+                </div>
+                <button class="yellow-btn" type="button">Get Now</button>
+            </div>
+            <div class="payment-plan-box flex-container">
+                <p>Business</p>
+                <p>$14.99/mo</p>
+                <p>for up to 2 editors and 3 projects</p>
+                <div class="paymen-plan-benefits">
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>3 projects</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>30-day version history</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Up to 2 editors</p>
+                    </div>
+                    <div class="plan-benefit flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Unlimited cloud storage</p>
+                    </div>
+                </div>
+                <button class="yellow-btn" type="button">Get Now</button>
+            </div>
+        </div>
+    </section>
+    <section class="testimonials">
+        <div class="container flex-container">
+            <p class="section-name">Testimonials</p>
+            <div class="pages-testimonials-t-p flex-container">
+                <h2 class="section-titles">We are very glad to get client review.</h2>
+                <p class="section-paragraph">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis.</p>
+            </div>
+            <div class="reviews my-slider flex-container ">
+                <div class="review flex-container">
+                    <div class= "review-content flex-container">
+                        <div class="review-img">
+                            <img src="images/person_head.png" alt="person_head">
+                        </div>
+                        <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat.</p>
+                        <p>Rudhi Sasmito</p>
+                        <p>Ketjap Abc</p>
+                    </div>
+                    <div class="colorful-stripe flex-container">
+                        <div class="yellow-stripe"></div>
+                        <div class="blue-stripe"></div>
+                    </div>
+                </div>
+                <div class="review flex-container">
+                    <div class= "review-content flex-container">
+                        <div class="review-img">
+                            <img src="images/person_head.png" alt="person_head">
+                        </div>
+                        <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat.</p>
+                        <p>Rudhi Sasmito</p>
+                        <p>Ketjap Abc</p>
+                    </div>
+                    <div class="colorful-stripe flex-container">
+                        <div class="yellow-stripe"></div>
+                        <div class="blue-stripe"></div>
+                    </div>
+                </div>
+                <div class="review flex-container">
+                    <div class= "review-content flex-container">
+                        <div class="review-img">
+                            <img src="images/person_head.png" alt="person_head">
+                        </div>
+                        <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat.</p>
+                        <p>Rudhi Sasmito</p>
+                        <p>Ketjap Abc</p>
+                    </div>
+                    <div class="colorful-stripe flex-container">
+                        <div class="yellow-stripe"></div>
+                        <div class="blue-stripe"></div>
+                    </div>
+                </div>
+                <div class="review flex-container">
+                    <div class= "review-content flex-container">
+                        <div class="review-img">
+                            <img src="images/person_head.png" alt="person_head">
+                        </div>
+                        <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat.</p>
+                        <p>Rudhi Sasmito</p>
+                        <p>Ketjap Abc</p>
+                    </div>
+                    <div class="colorful-stripe flex-container">
+                        <div class="yellow-stripe"></div>
+                        <div class="blue-stripe"></div>
+                    </div>
+                </div>
+                <div class="review flex-container">
+                    <div class= "review-content flex-container">
+                        <div class="review-img">
+                            <img src="images/person_head.png" alt="person_head">
+                        </div>
+                        <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat.</p>
+                        <p>Rudhi Sasmito</p>
+                        <p>Ketjap Abc</p>
+                    </div>
+                    <div class="colorful-stripe flex-container">
+                        <div class="yellow-stripe"></div>
+                        <div class="blue-stripe"></div>
+                    </div>
+                </div>
+                <div class="review flex-container">
+                    <div class= "review-content flex-container">
+                        <div class="review-img">
+                            <img src="images/person_head.png" alt="person_head">
+                        </div>
+                        <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat.</p>
+                        <p>Rudhi Sasmito</p>
+                        <p>Ketjap Abc</p>
+                    </div>
+                    <div class="colorful-stripe flex-container">
+                        <div class="yellow-stripe"></div>
+                        <div class="blue-stripe"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="logoipsum-learn-more flex-container">
+        <div class="container logoipsum-logos flex-container">
+            <img src="images/logoipsum logos/logoipsum 1.png" alt="logoipsum_logo_1">
+            <img src="images/logoipsum logos/logoipsum 2.png" alt="logoipsum_logo_2">
+            <img src="images/logoipsum logos/logoipsum 3.png" alt="logoipsum_logo_3">
+            <img src="images/logoipsum logos/logoipsum 4.png" alt="logoipsum_logo_4">
+        </div>
+        <div class="learn-more-consultant flex-container">
+            <p>looking for a first-class business consultant?</p>
+            <button class="yellow-btn" type="button">Learn More</button>
+        </div>
+    </section>
