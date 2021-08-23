5634a25a8fa805e3e9c00729023d90a13f4cb155
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 10:24:24 +0300

    Add files via upload
    Boo's project code of page.php

---
+++ pages.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boos</title>
+    <script src="https://kit.fontawesome.com/b090be4e4c.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,400&family=Poppins:wght@200;400;500&family=Roboto:ital,wght@0,400;0,500;0,700;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="css/normalize.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
+    <link href="css/style.css?v=<?php echo time(); ?>" rel="stylesheet">
+</head>
+<body id="pages-projects">
+    <?php
+        include_once 'views/pages/header.php';
+        include_once 'views/pages/content.php';
+        include_once 'views/pages/footer.php';
+    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/boos_js.js" type="text/javascript"></script>
+</body>
+</html>
