adb4b5876ca7218d087dbc4182dc88dc37c23ab3
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 10:36:50 +0300

    Add files via upload

---
+++ views/index/content.php
@@ -0,0 +1,282 @@
+<section class="about-section">
+        <div class="container flex-container">
+            <div class="about-part flex-container">
+                <p id="About" class="section-name">About</p>
+                <h2 class="section-titles">The most succesful entrepreneurs i know are optimistic. It's a part of the job description.</h2>
+                <p class="section-paragraph">Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.. Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                <button class="yellow-btn" type="button">Learn More</button>
+            </div>
+            <div class="about-graphics">
+                <div class="box dots"></div>
+                <div class="box blue-gs"> </div>
+                <div class="box grey-gs"></div>
+                <div class="box yellow-gs"></div>
+            </div>
+        </div>
+    </section>
+    <section class="services-section">
+        <div class="services-left-corner">
+        <div class="container flex-container">
+            <p id="Services" class="section-name">Services</p>
+            <div class="services-title flex-container">
+                <h2 class="section-titles">We help clients solve complex problems</h2>
+                <p class="section-paragraph">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis.</p>
+            </div>
+            <div class="different-services flex-container">
+                <div class="service flex-container">
+                    <img src="images/frame2.png" alt="Looking_glass">
+                    <div class="text">
+                        <h3>Financial Analysis</h3>
+                        <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+                <div class="service flex-container">
+                    <img src="images/frame1.png" alt="Lamp">
+                    <div class="text">
+                        <h3>Business Solutions</h3>
+                        <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+                <div class="service flex-container">
+                    <img src="images/frame5.png" alt="Shirt">
+                    <div class="text">
+                        <h3>Client Management</h3>
+                        <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="different-services flex-container">
+                <div class="service flex-container">
+                    <img src="images/frame6.png" alt="Money_Bubble">
+                    <div class="text">
+                        <h3>Online Consulting</h3>
+                        <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+                <div class="service flex-container">
+                    <img src="images/frame3.png" alt="Rocket">
+                    <div class="text">
+                        <h3>Business Opportunities</h3>
+                        <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+                <div class="service flex-container">
+                    <img src="images/frame4.png" alt="Monitor">
+                    <div class="text">
+                        <h3>IT Consulting</h3>
+                        <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+        <section class="statistics-section">
+            <div class="container flex-container">
+                <div class="statistics-graphics flex-container">
+                    <div class="dots"></div>
+                    <div class="both-squares flex-container">
+                        <div class="yellow-sq square flex-container">
+                            <p>734</p>
+                            <p>Satisfied Customers</p>
+                        </div>
+                        <div class="blue-sq square flex-container">
+                            <p>17</p>
+                            <p>Years Experience</p>
+                        </div>
+                    </div>
+                    <div class="both-squares flex-container">
+                        <div class="blue-sq square flex-container">
+                            <p>542</p>
+                            <p>Finance help</p>
+                        </div>
+                        <div class="yellow-sq square flex-container">
+                            <p>324</p>
+                            <p>Consulteds</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="statistics-text flex-container">
+                    <p class="section-name">Statistics</p>
+                    <h2 class="section-titles">We help clients solve complex problems</h2>
+                    <p class="section-paragraph">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis.</p>
+                    <div class="check-marks flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Excepteur</p>
+                    </div>
+                    <div class="check-marks flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Reprehenderit in voluptate</p>
+                    </div>
+                    <div class="check-marks flex-container">
+                        <i class="fas fa-check-circle"></i>
+                        <p>Consectetur adipisicing dolore.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <section class="recent">
+    <div class="container flex-container">
+        <p class="section-name">Recent Works</p>
+        <h2 class="section-titles">Features Project</h2>
+        <div class="features flex-container">
+            <div class="project-window flex-container">
+                <img src="images/BusinessGrowth.jpg" alt="BusinessGrowth">
+                <p>Business Growth</p>
+                <p>Coaching</p>
+            </div>
+            <div class="project-window flex-container">
+                <img src="images/data_analytics.jpg" alt="Data_Analytics">
+                <p>Data Analytics</p>
+                <p>Coaching</p>
+            </div>
+            <div class="project-window flex-container">
+                <img src="images/Revenue_Growth.jpg" alt="Revenue_Growth">
+                <p>Revenue Growth</p>
+                <p>Coaching</p>
+            </div>
+        </div>
+        <div class="features flex-container ">
+                <div class="project-window flex-container">
+                    <img src="images/BusinessGrowth.jpg" alt="BusinessGrowth">
+                    <p>Business Growth</p>
+                    <p>Coaching</p>
+                </div>
+                <div class="project-window flex-container">
+                    <img src="images/data_analytics.jpg" alt="Data_Analytics">
+                    <p>Data Analytics</p>
+                    <p>Coaching</p>
+                </div>
+                <div class="project-window flex-container">
+                    <img src="images/Revenue_Growth.jpg" alt="Revenue_Growth">
+                    <p>Revenue Growth</p>
+                    <p>Coaching</p>
+                </div>
+        </div>
+    </div>
+    </section>
+    <section class="testimonials">
+        <div class="container flex-container">
+            <p class="section-name">Testimonials</p>
+            <h2 class="section-titles">We are very glad to get client review.</h2>
+            <p class="section-paragraph">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis.</p>
+            <div class="reviews my-slider flex-container ">
+                    <div class="review flex-container">
+                        <div class="review-content">
+                            <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>Rudhi Sasmito</p>
+                            <p>Ketjap Abc</p>
+                        </div>
+                    </div>
+                    <div class="review flex-container">
+                        <div class="review-content">
+                            <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>Rudhi Sasmito</p>
+                            <p>Ketjap Abc</p>
+                        </div>
+                    </div>
+                    <div class="review flex-container">
+                        <div class="review-content">
+                            <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent.</p>
+                            <p>Rudhi Sasmito</p>
+                            <p>Ketjap Abc</p>
+                        </div>
+                    </div>
+                    <div class="review flex-container">
+                        <div class="review-content">
+                            <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p>Voluptatem accusantium dolore mque laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>Rudhi Sasmito</p>
+                            <p>Ketjap Abc</p>
+                        </div>
+                    </div>
+                    <div class="review flex-container">
+                        <div class="review-content">
+                            <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p>Laudantium totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>Rudhi Sasmito</p>
+                            <p>Ketjap Abc</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <section class="news-section">
+        <div class="container flex-container">
+            <p id="News" class="section-name">News</p>
+            <h2 class="section-titles">Popular Articles And Tips</h2>
+            <div class="news-articles flex-container">
+                <div class="article flex-container">
+                    <img src="images/designbig.jpg" alt="design">
+                    <p>Reprehenderit in voluptate velit esse cillum</p>
+                    <div class="article-date-type flex-container">
+                        <p>July 29, 2020</p>
+                        <p>Arts</p>
+                    </div>
+                    <p class="addReadMore showlesscontent">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.</p>
+                </div>
+                <div class="article flex-container">
+                    <img src="images/designbig.jpg" alt="design">
+                    <p>Reprehenderit in voluptate velit esse cillum</p>
+                    <div class="article-date-type flex-container">
+                        <p>July 29, 2020</p>
+                        <p>Arts</p>
+                    </div>
+                    <p class="addReadMore showlesscontent">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.</p>
+                </div>
+                <div class="article flex-container">
+                    <img src="images/designbig.jpg" alt="design">
+                    <p>Reprehenderit in voluptate velit esse cillum</p>
+                    <div class="article-date-type flex-container">
+                        <p>July 29, 2020</p>
+                        <p>Arts</p>
+                    </div>
+                    <p class="addReadMore showlesscontent">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.</p>
+            </div>
+        </div>
+    </section>
+    <section class="request-quote-section">
+        <div class="container flex-container">
+            <div class="quote-title flex-container">
+                <h2 class="section-titles">Request a Free Quote</h2>
+                <p class="section-paragraph">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis.</p>
+            </div>
+            <form id="contact-form" class="flex-container" action="index.php" method="post">
+                <div class="form-title">
+                    <p>Request a Free Quote</p>
+                </div>
+                <input type="text" name="name" placeholder="Your Name" required>
+                <input type="email" name="email" placeholder="Your Email" required>
+                <textarea name="message" rows="8" placeholder="Your Message" required></textarea>
+                <input class="btn btn-form" type="submit" name="Submit">
+            </form>
+        </div>
+    </section>
---
+++ views/index/header.php
@@ -0,0 +1,63 @@
+<header class="top-header">
+        <div class="top-bar flex-container">
+            <div class="hours">
+                <p>We're 24/7 Hours Service Provider</p>
+            </div>
+            <div class="top-contacts">
+                <ul class="top-bar-ul flex-container">
+                    <li class="contact-list">
+                        <i class="fas fa-envelope"></i>
+                        <a href="mailto:support@boos.com">support@boos.com</a>
+                    </li>
+                    <li class="contact-list">
+                        <i class="fas fa-phone"></i>
+                        <a href="tel:621234678010">(+62) 1234 678 010</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section class="hero">
+        <header class="site-header">
+            <div class="container flex-container">
+                <div class="logo">
+                    <img src="images/logo.png" alt="Boos">
+                </div>
+                <nav class="main-nav">
+                    <ul class="flex-container">
+                        <li><a href="#">Home +</a></li>
+                        <li ><a class="About" href="#">About</a></li>
+                        <li><a class="Services" href="#">Services +</a></li>
+                        <li><a href="news.php">News</a></li>
+                        <li><a href="pages.php">Pages +</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+                <nav class="mob-nav">
+                    <ul id="mobMenu" class="flex-container">
+                        <li><a href="#">Home +</a></li>
+                        <li><a class="About" href="#">About</a></li>
+                        <li><a class="Services" href="#">Services +</a></li>
+                        <li><a href="news.php">News</a></li>
+                        <li><a href="pages.php">Pages +</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                    <a href="javascript:void(0);" class="icon" onclick="displayMobileNav()">
+                        <i class="fa fa-bars"></i>
+                    </a>
+                </nav>
+                <div class="search flex-container">
+                    <textarea id="search-bar" name="message" rows="1" placeholder="Search here..."></textarea>
+                    <i class="fas fa-search" onclick="displaySearchBar()"></i>
+                </div>
+            </div>
+    </header>
+        <div class="container flex-container">
+            <h1>Together we're building brighter futures</h1>
+            <p>Our professional team works to increase productivity and cost effectiviness on the market</p>
+            <div class="hero-buttons">
+                <button class="blue-btn" type="button">Learn More</button>
+                <button class="yellow-btn" type="button">Learn More</button>
+            </div>
+        </div>
+    </section>
